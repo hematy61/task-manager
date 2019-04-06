960b51cb2d174d42960a77cc71a882c5c1169ff5
Mohammad Hematy <hematy61@gmail.com>
2019-04-05 23:52:01 -0500

    set up a post route to test the app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`)
