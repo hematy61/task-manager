7b553968b1d40fb26736aa127445e1a9fd7efd03
Mohammad Hematy <hematy61@gmail.com>
2019-04-07 00:13:50 -0500

    add the route to find a task by it's id

--- src/index.js
+++ src/index.js
@@ -42,6 +42,13 @@ app.get('/tasks', (req, res) => {
   .catch( (error) => res.status(400).send(error) )
 })
 
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id
+  Task.findById(_id)
+  .then( (task) => !task ? res.status(404).send() : res.send(task) )
+  .catch( error => res.status(500).send() )
+})
+
 app.listen(port, () => {
   console.log(`server is running on port ${port}`)
 })
