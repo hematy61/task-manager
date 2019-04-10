6bc9b6c7aa26a4cecf731910a448d95457c863df
Mohammad Hematy <hematy61@gmail.com>
2019-04-09 20:17:51 -0500

    add async/await to the /users/:id route get request

--- src/index.js
+++ src/index.js
@@ -28,11 +28,13 @@ app.get('/users', async (req, res) => {
   }
 })
 
-app.get('/users/:id', (req,res) => {
-  const _id = req.params.id
-  User.findById(_id)
-  .then( user => !user ? res.status(404).send() : res.send(user) )
-  .catch( error => res.status(500).send() )
+app.get('/users/:id', async (req,res) => {
+  try {      
+      const user = await User.findById(req.params.id)
+      !user ? res.status(404).send() : res.send(user)
+  } catch (error) {
+      res.status(500).send(error)
+  }
 })
 
 app.post('/tasks', (req, res) => {
