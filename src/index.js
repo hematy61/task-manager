7e2c94e157be59afd33f23f83858e9c4199e961d
Mohammad Hematy <hematy61@gmail.com>
2019-04-11 20:03:14 -0500

    make user routes file to make routes modular

--- src/index.js
+++ src/index.js
@@ -1,92 +1,15 @@
 const express = require('express')
-const { ObjectID } = require('mongodb')
 require('./db/mongoose')
-const User = require('./models/user')
 const Task = require('./models/task')
+const userRouter = require('./routes/userRoutes')
+
 
 const app = express()
 const port = process.env.PORT || 3000
 
 // supporting static files with Express
 app.use(express.json())
-
-// *************  CREATE A NEW USER  *************************************************************
-// With this route we are using async await to asynchronously create new users and save them in
-// users database. Uppercase "User" is the mongoose model for user authentication. 
-app.post('/users', async (req, res) => {
-  const user = new User(req.body)
-  try {
-      await user.save()
-      res.status(201).send(user)
-  } catch (error) {
-      res.status(400).send(error)
-  }
-})
-
-// *************  FIND ALL USERS  ****************************************************************
-// With this route we are using async await to asynchronously retrieve all users and send them back
-// to front end. Uppercase "User" is the mongoose model for user authentication. 
-app.get('/users', async (req, res) => {
-  try {
-      const users = await User.find({})
-      res.send(users)
-  } catch (error) {
-      res.status(500).send(error)
-  }
-})
-
-// *************  FIND A USER BY ID  *************************************************************
-// With this route we are using async await to asynchronously find one user by its id and send it 
-// back to front end. Uppercase "User" is the mongoose model for user authentication. 
-app.get('/users/:id', async (req,res) => {
-  try {      
-      const user = await User.findById(req.params.id)
-      return !user ? res.status(404).send(`The id: ${req.params.id} was not found.`) : res.send(user)
-  } catch (error) {
-      res.status(500).send(error)
-  }
-})
-
-// *************  UPDATE A USER BY ID  ***********************************************************
-// With this route we are using async await to asynchronously find one user by its id and update the 
-// user's information. Uppercase "User" is the mongoose model for user authentication. 
-app.patch('/users/:id', async (req, res) => {
-  // Here, we want to check the requested update. If it's an invalid request like updating a key that 
-  // is not exist in user keys then we want to stop the request and send an "invalid update" message to
-  // client. 'requestedUserUpdates are the keys of the object that has received from client to update user'
-  const requestedUserUpdates = Object.keys(req.body);
-  // 'allowedUserUpdates' are the keys that we would like client to be able to update, otherwise stop it.
-  const allowedUserUpdates = ['name', 'age', 'email', 'password']
-  // With 'isValidUpdateOperation, we want to see 'requestedUserUpdates' keys are valid within the 
-  // 'allowedUserUpdates' and if there is then it can be updated. otherwise reject the update.
-  const isValidUpdateOperation = requestedUserUpdates.every(update => allowedUserUpdates.includes(update))
-  if (!isValidUpdateOperation) {
-    return res.status(400).send('Invalid update!')
-  }
-
-  try {
-      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-      })
-      // if findByIdAndUpdate doesn't find any record then is gonna return false. With this ternary 
-      // operator we can check user variable and if it returns false then turn it to true by ! operator
-      // and sent the error to client
-      return !user ? res.status(400).send() : res.send(['Modified user: ',user]) 
-  } catch (error) {
-      res.status(500).send(error)
-  }
-})
-
-// *************  DELETE A  USER  *****************************************************************
-app.delete('/users/:id', async (req, res) => {
-  try {
-      const deletedUser = await User.findByIdAndDelete(req.params.id)
-      return !deletedUser ? res.status(404).send('Invalid User!') : res.send(deletedUser)
-  } catch (error) {
-      res.status(404).send(error)
-  }
-})
+app.use(userRouter)
 
 
 // *************  CREATE A NEW TASK  *************************************************************
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const User = require('../models/user')
+const router = express.Router()
+
+
+// *************  CREATE A NEW USER  *************************************************************
+// With this route we are using async await to asynchronously create new users and save them in
+// users database. Uppercase "User" is the mongoose model for user authentication. 
+router.post('/users', async (req, res) => {
+  const user = new User(req.body)
+  try {
+      await user.save()
+      res.status(201).send(user)
+  } catch (error) {
+      res.status(400).send(error)
+  }
+})
+
+// *************  FIND ALL USERS  ****************************************************************
+// With this route we are using async await to asynchronously retrieve all users and send them back
+// to front end. Uppercase "User" is the mongoose model for user authentication. 
+router.get('/users', async (req, res) => {
+  try {
+      const users = await User.find({})
+      res.send(users)
+  } catch (error) {
+      res.status(500).send(error)
+  }
+})
+
+// *************  FIND A USER BY ID  *************************************************************
+// With this route we are using async await to asynchronously find one user by its id and send it 
+// back to front end. Uppercase "User" is the mongoose model for user authentication. 
+router.get('/users/:id', async (req, res) => {
+  try {
+      const user = await User.findById(req.params.id)
+      return !user ? res.status(404).send(`The id: ${req.params.id} was not found.`) : res.send(user)
+  } catch (error) {
+      res.status(500).send(error)
+  }
+})
+
+// *************  UPDATE A USER BY ID  ***********************************************************
+// With this route we are using async await to asynchronously find one user by its id and update the 
+// user's information. Uppercase "User" is the mongoose model for user authentication. 
+router.patch('/users/:id', async (req, res) => {
+  // Here, we want to check the requested update. If it's an invalid request like updating a key that 
+  // is not exist in user keys then we want to stop the request and send an "invalid update" message to
+  // client. 'requestedUserUpdates are the keys of the object that has received from client to update user'
+  const requestedUserUpdates = Object.keys(req.body);
+  // 'allowedUserUpdates' are the keys that we would like client to be able to update, otherwise stop it.
+  const allowedUserUpdates = ['name', 'age', 'email', 'password']
+  // With 'isValidUpdateOperation, we want to see 'requestedUserUpdates' keys are valid within the 
+  // 'allowedUserUpdates' and if there is then it can be updated. otherwise reject the update.
+  const isValidUpdateOperation = requestedUserUpdates.every(update => allowedUserUpdates.includes(update))
+  if (!isValidUpdateOperation) {
+    return res.status(400).send('Invalid update!')
+  }
+
+  try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+      })
+      // if findByIdAndUpdate doesn't find any record then is gonna return false. With this ternary 
+      // operator we can check user variable and if it returns false then turn it to true by ! operator
+      // and sent the error to client
+      return !user ? res.status(400).send() : res.send(['Modified user: ', user])
+  } catch (error) {
+      res.status(500).send(error)
+  }
+})
+
+// *************  DELETE A  USER  *****************************************************************
+router.delete('/users/:id', async (req, res) => {
+  try {
+      const deletedUser = await User.findByIdAndDelete(req.params.id)
+      return !deletedUser ? res.status(404).send('Invalid User!') : res.send(deletedUser)
+  } catch (error) {
+      res.status(404).send(error)
+  }
+})
+
+
+module.exports = router
