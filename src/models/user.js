7a8daa96609564238669888148e77da045047653
Mohammad Hematy <hematy61@gmail.com>
2019-04-12 20:19:43 -0500

    add userSchema and define pre save middleware to bcrypt passwords before saving to database

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
+const bcrypt = require('bcryptjs')
 
 
-
-const User = mongoose.model('User',{
+const userSchema = new mongoose.Schema({
   // if you add a property here make sure that you're going to update 'allowedUserUpdates' array on
   // index.js file within 'UPDATE A USER BY ID' route
   name: {
@@ -16,8 +16,8 @@ const User = mongoose.model('User',{
     required: [true, 'User email required'],
     trim: true,
     lowercase: true,
-    validate(value){
-      if ( !isEmail(value) ) {
+    validate(value) {
+      if (!isEmail(value)) {
         throw new Error('Enter a valid email')
       }
     }
@@ -27,7 +27,7 @@ const User = mongoose.model('User',{
     required: true,
     minlength: [7, 'Minimum 7 characters required'],
     trim: true,
-    validate(value){
+    validate(value) {
       if (value.toLowerCase().includes('password')) {
         throw new Error('password cannot contain the keyword "password"')
       }
@@ -36,12 +36,26 @@ const User = mongoose.model('User',{
   age: {
     type: Number,
     default: 0,
-    validate(value){
-      if ( value < 18 ) {
+    validate(value) {
+      if (value < 18) {
         throw new Error('You must be over 18 to use this service.')
       }
     }
   }
 })
+
+userSchema.pre('save', async function (next) {
+  // In document middleware functions, "this" refers to the document.
+  const user = this
+  console.log(user.password)
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+    console.log(user.password)
+  }
+
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
