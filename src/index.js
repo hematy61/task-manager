f97730afa75578fe07b9e1221b440f212bf01b84
Mohammad Hematy <hematy61@gmail.com>
2019-04-30 21:14:21 -0500

    add cmd-env for cross platform environment variables support and added PORT variable to dev.env file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const taskRouter = require('./routes/taskRoutes')
 
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 // supporting static files with Express
 app.use(express.json())
