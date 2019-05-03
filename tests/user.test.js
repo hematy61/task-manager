f645ab2d8de67607f3f9bcb24f176277980e9081
Mohammad Hematy <hematy61@gmail.com>
2019-05-02 22:47:43 -0500

    add existing user login test

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,8 +2,16 @@ const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOne = {
+  name: 'Andy',
+  email: 'andyTest@test.ca',
+  password: 'TestCase1!!',
+  age: 20
+}
+
 beforeEach( async () => {
   await User.deleteMany()
+  await new User(userOne).save()
 })
 
 test('should sign up a new user', async () => {
@@ -11,6 +19,14 @@ test('should sign up a new user', async () => {
     name: 'Mo',
     email: 'moTest1@test.com',
     password: 'SomePass12!!',
-    age: 34
+    age: 19
   }).expect(307)
+})
+
+test('Should login user', async () => {
+  await request(app).post('/users/login').send({
+    email: userOne.email,
+    password: userOne.password
+  })
+  .expect(200)
 })
