d6cdb44dac9f1ce82b195356145858eb1bf396f1
Mohammad Hematy <hematy61@gmail.com>
2019-05-02 23:23:21 -0500

    add test code for getting user profile request

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,19 @@
 const request = require('supertest')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+  _id: userOneId,
   name: 'Andy',
   email: 'andyTest@test.ca',
   password: 'TestCase1!!',
-  age: 20
+  age: 20,
+  tokens: [{
+    token: jwt.sign({ _id: userOneId}, process.env.JWT_SECRET)
+  }]
 }
 
 beforeEach( async () => {
@@ -37,4 +44,12 @@ test('Should not login nonexistent user', async () => {
     password: 'notExist'
   })
   .expect(400)
+})
+
+test('should get user profile', async () => {
+  await request(app)
+          .get('/users/me')
+          .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+          .send()
+          .expect(200)
 })
