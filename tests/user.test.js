4b13304b88b88c59b20467f9bf3d2786af939562
Mohammad Hematy <hematy61@gmail.com>
2019-05-08 20:59:31 -0500

    add more test assertions on logged in user and makeing sure that it is saved correctly in database and check removing of the user after deleting the account

--- tests/user.test.js
+++ tests/user.test.js
@@ -8,7 +8,7 @@ const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
   _id: userOneId,
   name: 'Andy',
-  email: 'andyTest@test.ca',
+  email: 'andytest@test.ca',
   password: 'TestCase1!!',
   age: 20,
   tokens: [{
@@ -31,11 +31,30 @@ test('should sign up a new user', async () => {
 })
 
 test('Should login user', async () => {
-  await request(app).post('/users/login').send({
+  const response = await request(app).post('/users/login').send({
     email: userOne.email,
     password: userOne.password
   })
   .expect(200)
+  // Assert that the database was changed correctly
+  const user = await User.findById(response.body.user._id)
+  expect(user).not.toBeNull()
+
+  // Assertions about the response
+  expect(response.body).toMatchObject({
+    user: {
+      name: 'Andy',
+      email: 'andytest@test.ca',
+      age: 20
+    },
+    token: user.tokens[0].token
+  })
+
+  // Assertion about user password not being stored as a plain text
+  expect(user.password).not.toBe('SomePass12!!')
+
+  // Assertion about user token being saved to database
+  expect(user.token).toBe(userOne.token)
 })
 
 test('Should not login nonexistent user', async () => {
@@ -62,11 +81,15 @@ test('Should not get profile for unauthenticated user', async () => {
 })
 
 test('Should delete user account', async () => {
-  await request(app)
+  const response = await request(app)
           .delete('/users/me')
           .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
           .send()
           .expect(200)
+
+  // Assertion about validating user successfully was removed
+  const user = await User.findById(userOneId)
+  expect(user).toBeNull()
 })
 
 test('Should not delete user for unauthenticated user', async () => {
